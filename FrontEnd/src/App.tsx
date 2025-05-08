772604e88a7214dbe52b2167567213aa83d099e7
Davide Ferri <davideferri2006@gmail.com>
2025-05-08 14:10:46 +0200

    sistemazione piccolezze stilistiche

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,21 +1,39 @@
 import {BrowserRouter,Routes,Route} from "react-router"
-import { Homepage } from './Pages/Homepage/Homepage.tsx'
+import Homepage from './Pages/Homepage/Homepage.tsx'
 import Access_account from './Pages/Manage_account/Access_account/Access_account.tsx'
 import Dashboard from './Pages/Dashboard/Dashboard.tsx'
 import School_activity from './Pages/School_activity/School_activity.tsx'
 import Personal_activity from './Pages/Personal_activity/Personal_activity.tsx'
 import About from "./Pages/About/About.tsx"
 import Help from "./Pages/Help/Help.tsx"
-
+import { useEffect, useState } from "react";
 
 function App() {
+
+
+  // Tutto questo serve per tener traccia del tempo passato dall'apertura dell'applicazione,
+  // in modo da far fare l'animazione dell'header solo all'inizio, e non ogni volta che si cambia pagina
+  const [secondsElapsed, setSecondsElapsed] = useState(0);
+  var done = false;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSecondsElapsed(prev => prev + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  if (secondsElapsed > 3) {
+    done = true;
+  }
 
   return (
    <div>
        <BrowserRouter>
           <Routes>
             <Route path="/AccessPage" element={<Access_account />} />
-            <Route path="/" element={<Homepage />} />
+            <Route path="/" element={<Homepage isDone={done}/>} />
             <Route path="/Dashboard" element= {<Dashboard/>} />
             <Route path="/PersonalActivity" element= {<Personal_activity/>} />
             <Route path="/SchoolActivity" element= {<School_activity/>} />
--- FrontEnd/src/Pages/Homepage/Homepage.tsx
+++ FrontEnd/src/Pages/Homepage/Homepage.tsx
@@ -7,12 +7,18 @@ import './Homepage.css'
 import Decorations  from './Decorations.tsx';
 import Header from '../../Components/Header/Header.tsx';
 
-export const Homepage = () => {
+interface props_homepage {
+    isDone?: boolean
+  }
+  
+const Homepage: React.FC<props_homepage> = ({isDone}) => {
+
   return (
     
+    isDone ? (
     <div className='homepage'>
         <div className='header-section'>
-            <Header animation={true}/>
+            <Header animation={false}/>
         </div>
         <div className='initial-section'>
             
@@ -63,7 +69,61 @@ export const Homepage = () => {
         </div>
     </div>
     </div>
-  )
+  ):( <div className='homepage'>
+    <div className='header-section'>
+        <Header animation={true}/>
+    </div>
+    <div className='initial-section'>
+        
+        <div className='bg-decoration'><Decorations />
+        
+            <div className='welcome'>
+                <Animated_text
+                    html_element='h2'
+                    className=''
+                    text= 'Benvenuto in 5F srl !'
+                />
+            </div>
+            </div>
+            <div className='access-button'>
+                <Link to='/AccessPage' style={{textDecoration:'none'}}>
+                    <button className='button-access'>
+                        Start Now
+                    </button>
+                </Link>
+            </div>
+
+            
+        <div className='functionality-section'>
+            <div className='Title'> <h3> Functionality </h3></div>
+            <div className='main-section-func'>
+                {Data_boxes.data.map((data,dataIndex) => (
+                    <Box_interactive 
+                    key={dataIndex}
+                    title={data.title}
+                    sub_text={data.sub_text}
+                    className = {data.styling}
+                    />
+                ))}
+        </div>
+        <div className='subtext-functionality'> 
+            <p>Migliora la tua organizzazione degli impegni per goderti un migliore e più lungo tempo libero</p>
+        </div>
+    </div>
+    <div className='about_us-section'>
+        <div className='Title'><h3>About us</h3></div>
+        <div className='main-section-about'>
+            About us ancora da definire in struttura e componenti e anche da implementare e stilizzare
+
+        </div>
+    </div>
+    <div className='footer-section'>
+        <Footer/>
+    </div>
+</div>
+</div>
+)
+)
 }
 
 export default Homepage;
