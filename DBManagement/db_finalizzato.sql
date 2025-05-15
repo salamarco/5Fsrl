fcbf28c332746e5293e2bdc2896ef406afb14335
Luca Cavaliere <80126112+SonLuke13@users.noreply.github.com>
2025-05-15 11:27:51 +0200

    Update db_finalizzato.sql
    Porco putano, dovrebbe essere finalizzato ora, se notate che qualcosa non va bene fatemelo sapere

--- DBManagement/db_finalizzato.sql
+++ DBManagement/db_finalizzato.sql
@@ -1,73 +1,66 @@
 CREATE DATABASE IF NOT EXISTS db_5f_srl;
+
 USE db_5f_srl;
 
--- Classi
-CREATE TABLE IF NOT EXISTS Classi(
-    id_classe INT AUTO_INCREMENT,
-    anno int NOT NULL,
-    sezione VARCHAR(255),
-    PRIMARY KEY(id_classe)
+CREATE TABLE IF NOT EXISTS Materie(
+    id_materia INT AUTO_INCREMENT,
+    nome_materia VARCHAR(255) UNIQUE NOT NULL,
+    
+    PRIMARY KEY(id_materia)
 );
 
--- Utenti
-CREATE TABLE IF NOT EXISTS Utenti (
-    username VARCHAR(255) NOT NULL UNIQUE,
-    email VARCHAR(255) UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
-    nome VARCHAR(255) NOT NULL,
-    cognome VARCHAR(255) NOT NULL,
-    id_classe INT,
-    PRIMARY KEY (username),
-    FOREIGN KEY(id_classe) REFERENCES Classi(id_classe) ON DELETE SET NULL
-);
-
--- Scuole
 CREATE TABLE IF NOT EXISTS Scuole(
     id_scuola INT AUTO_INCREMENT,
-    nome_scuola VARCHAR(255) NOT NULL UNIQUE,
-    numero_anni INT NOT NULL,
+    nome_scuola VARCHAR() NOT NULL UNIQUE,
+    n_scuola INT NOT NULL,
+    
     PRIMARY KEY(id_scuola)
 );
 
--- Indirizzi
 CREATE TABLE IF NOT EXISTS Indirizzi(
     id_indirizzo INT AUTO_INCREMENT,
     nome_indirizzo VARCHAR(255) NOT NULL,
     id_scuola INT NOT NULL,
+    anno_indirizzo INT NOT NULL,
+    
     PRIMARY KEY(id_indirizzo),
-    FOREIGN KEY(id_scuola) REFERENCES Scuole(id_scuola) ON DELETE CASCADE
+    FOREIGN KEY(id_scuola) REFERENCES Scuole(id_scuola) ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
--- Materie
-CREATE TABLE IF NOT EXISTS Materie(
-    id_materia INT AUTO_INCREMENT,
-    nome_materia VARCHAR(255) NOT NULL UNIQUE,
-    PRIMARY KEY(id_materia)
-);
+CREATE TABLE IF NOT EXISTS Utenti(
+    email VARCHAR(255),
+    username VARCHAR(255),
+    password_hash VARCHAR(255) NOT NULL,
+    id_indirizzo INT NOT NULL,
+    anno_scuola INT NOT NULL,
 
--- Lista_materie_indirizzo
+    PRIMARY KEY(email),
+    FOREIGN KEY(id_indirizzo) REFERENCES Indirizzi(id_indirizzo) ON UPDATE CASCADE ON DELETE SET DEFAULT NULL,
+    FOREIGN KEY(anno_scuola) REFERENCES Indirizzi(anno_indirizzo) ON UPDATE CASCADE ON DELETE SET DEFAULT NULL
+
+)
+
 CREATE TABLE IF NOT EXISTS Lista_materie_indirizzo(
     id_materie_indirizzo INT AUTO_INCREMENT,
-    id_indirizzo INT NOT NULL,
-    id_materia INT NOT NULL,
+    id_indirizzo INT,
+    nome_materia VARCHAR(255) NOT NULL,
     ore_settimanali INT NOT NULL,
-    anno INT NOT NULL CHECK (anno BETWEEN 1 AND 5),
+    
     PRIMARY KEY(id_materie_indirizzo),
-    FOREIGN KEY(id_indirizzo) REFERENCES Indirizzi(id_indirizzo) ON DELETE CASCADE,
-    FOREIGN KEY(id_materia) REFERENCES Materie(id_materia) ON DELETE CASCADE,
-    UNIQUE KEY unique_materia_indirizzo_anno (id_indirizzo, id_materia, anno)
+    FOREIGN KEY(nome_materia) REFERENCES Materie(nome_materia) ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
--- Lezioni
 CREATE TABLE IF NOT EXISTS Lezioni(
     id_lezione INT AUTO_INCREMENT,
+    id_utente VARCHAR(255) NOT NULL,
     data_inizio_lezione DATETIME NOT NULL,
     data_fine_lezione DATETIME NOT NULL,
     id_materia INT NOT NULL,
-    id_classe INT NOT NULL,
+    
     PRIMARY KEY(id_lezione),
-    FOREIGN KEY(id_classe) REFERENCES Classi(id_classe) ON DELETE CASCADE,
-    FOREIGN KEY(id_materia) REFERENCES Materie(id_materia) ON DELETE CASCADE);
+    FOREIGN KEY(id_materia) REFERENCES Lista_materie_indirizzo(id_materie_indirizzo) ON UPDATE CASCADE ON DELETE SET DEFAULT NULL,
+    FOREIGN KEY(id_utente) REFERENCES Utenti(email) ON UPDATE CASCADE ON DELETE RESTRICT,
+
     CONSTRAINT l_check_duration 
         CHECK (
             TIMESTAMPDIFF(MINUTE, data_inizio_lezione, data_fine_lezione) >= 50 AND
@@ -77,55 +70,76 @@ CREATE TABLE IF NOT EXISTS Lezioni(
         )
 );
 
--- Compiti
-CREATE TABLE IF NOT EXISTS Compiti(
-    id_compito INT AUTO_INCREMENT,
-    titolo VARCHAR(255) NOT NULL,
-    data_consegna DATETIME NOT NULL,
-    id_materia INT NOT NULL,
-    id_classe INT NOT NULL, 
-    stato_compito ENUM('Da iniziare','In Corso','Completato') NOT NULL DEFAULT 'Da iniziare',
-    priorita_compito ENUM('Alta','Media','Bassa') NOT NULL DEFAULT 'Media',
-    descrizione TEXT,
-    PRIMARY KEY(id_compito),
-    FOREIGN KEY(id_materia) REFERENCES Materie(id_materia) ON DELETE CASCADE,
-    FOREIGN KEY(id_classe) REFERENCES Classi(id_classe) ON DELETE CASCADE
-);
-
--- Verifiche
 CREATE TABLE IF NOT EXISTS Verifiche(
     id_verifica INT AUTO_INCREMENT,
     data_verifica DATETIME NOT NULL,
-    id_classe INT NOT NULL,
+    id_utente VARCHAR(255) NOT NULL,
     id_materia INT NOT NULL,
     tipologia_verifica ENUM('Scritta','Orale','Progetto') NOT NULL,
     descrizione TEXT,
 
     PRIMARY KEY(id_verifica),
-    FOREIGN KEY(id_materia) REFERENCES Materie(id_materia),
-    FOREIGN KEY(id_classe) REFERENCES Classi(id_classe)
+    FOREIGN KEY(id_materia) REFERENCES Lista_materie_indirizzo(id_materie_indirizzo) ON UPDATE CASCADE ON DELETE SET DEFAULT NULL,
+    FOREIGN KEY(id_utente) REFERENCES Utenti(email) ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
--- Tipologie_personali
-CREATE TABLE IF NOT EXISTS Tipologie_personali (
+CREATE TABLE IF NOT EXISTS Compiti(
+    id_compito INT AUTO_INCREMENT,
+    titolo VARCHAR(255) NOT NULL,
+    data_consegna DATETIME NOT NULL,
+    id_materia INT NOT NULL,
+    id_utente VARCHAR(255) NOT NULL,
+    stato_compito ENUM('Da iniziare','In Corso','Completato') NOT NULL,
+    priority_compito ENUM('Alta','Media','Bassa')  NOT NULL,
+    
+    PRIMARY KEY(id_compito),
+    FOREIGN KEY(id_materia_compito) REFERENCES Lista_materie_indirizzo(id_materie_indirizzo) ON UPDATE CASCADE ON DELETE SET DEFAULT NULL,
+    FOREIGN KEY(id_utente) REFERENCES Utenti(email) ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE IF NOT EXISTS Tipologie_personali(
     id_tipologia INT AUTO_INCREMENT,
-    username VARCHAR(255) NOT NULL,
+    id_utente VARCHAR(255) NOT NULL,
     nome_tipologia VARCHAR(255) NOT NULL,
     colore_tipologia ENUM('#ab7050','#ed8d2d','#dec735','#19a850','#1e5eba','#e6443c','#b04a84','#7542a8') NOT NULL,
+
     PRIMARY KEY(id_tipologia),
-    FOREIGN KEY(username) REFERENCES Utenti(username) ON DELETE CASCADE,
-    UNIQUE KEY unique_tipologia_per_utente (username, nome_tipologia)
+    FOREIGN KEY(id_utente) REFERENCES Utenti(email) ON UPDATE CASCADE ON DELETE RESTRICT
+);
+    
+CREATE TABLE IF NOT EXISTS Attivita_personali(
+    id_attivita_personale INT AUTO_INCREMENT,
+    titolo VARCHAR(255) NOT NULL,
+    id_utente VARCHAR(255) NOT NULL,
+    data_inizio DATETIME NOT NULL,
+    data_fine DATETIME NOT NULL,
+    descrizione TEXT,
+    tipologia_attivita VARCHAR(255),
+    CONSTRAINT a_check_duration 
+        CHECK (
+            DATE(data_fine) > DATE(data_inizio)
+        ),
+    
+    PRIMARY KEY(id_attivita_personale),
+    FOREIGN KEY(id_utente) REFERENCES Utenti(email) ON UPDATE CASCADE ON DELETE RESTRICT,
+    FOREIGN KEY(tipologia_attivita) REFERENCES Tipologie_personali(nome_tipologia) ON UPDATE CASCADE ON DELETE SET DEFAULT NULL
 );
 
--- Eventi_Personali
-CREATE TABLE IF NOT EXISTS Eventi_Personali(
-    id_evento INT AUTO_INCREMENT,
-    titolo VARCHAR(255) NOT NULL,
-    username VARCHAR(255) NOT NULL,
-    data_ora DATETIME NOT NULL,
-    descrizione TEXT,
-    id_tipologia INT NOT NULL,
-    PRIMARY KEY(id_evento),
-    FOREIGN KEY(username) REFERENCES Utenti(username) ON DELETE CASCADE,
-    FOREIGN KEY(id_tipologia) REFERENCES Tipologie_personali(id_tipologia) ON DELETE CASCADE
-);
+-- COMANDI DI INSERIMENTO DATI VANNO INSERITI IN UN ALTRO FILE
+INSERT INTO Scuole(nome_scuola),
+VALUES('Liceo');
+
+INSERT INTO Scuole(nome_scuola),
+VALUES('Istituo tecnico');
+
+INSERT INTO Scuole(nome_scuola),
+VALUES('Professionale');
+
+INSERT INTO Scuole(nome_scuola),
+VALUES('ieFP triennale');
+
+INSERT INTO Scuole(nome_scuola),
+VALUES('ieFP quadriennale');
+
+INSERT INTO Scuole(nome_scuola),
+VALUES('Scuole medie');
